428fba7d64aac99a7a558d23943b7b7bc3052e38
akaMiWP <bangkokappdevelopment@gmail.com>
2024-11-23 17:25:10 +0700

    Followed along tutorials

--- puppet/programs/puppet/src/lib.rs
+++ puppet/programs/puppet/src/lib.rs
@@ -5,7 +5,8 @@ declare_id!("99Xn7gxzNuFjm86MhfR4m735zc7Mk8Hs8Nc6tUD843o6");
 #[program]
 pub mod puppet {
     use super::*;
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, authority: Pubkey) -> Result<()> {
+        ctx.accounts.puppet.authority = authority;
         Ok(())
     }
 
@@ -16,10 +17,9 @@ pub mod puppet {
     }
 }
 
-
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 8)]
+    #[account(init, payer = user, space = 8 + 8 + 32)]
     pub puppet: Account<'info, Data>,
     #[account(mut)]
     pub user: Signer<'info>,
@@ -28,11 +28,13 @@ pub struct Initialize<'info> {
 
 #[derive(Accounts)]
 pub struct SetData<'info> {
-    #[account(mut)]
+    #[account(mut, has_one = authority)]
     pub puppet: Account<'info, Data>,
+    pub authority: Signer<'info>,
 }
 
 #[account]
 pub struct Data {
     pub data: u64,
+    pub authority: Pubkey,
 }
